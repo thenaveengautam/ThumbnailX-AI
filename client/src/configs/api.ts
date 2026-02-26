789a5cd0853b7c99d72ee51f0496df41edd7960a
thenaveengautam <dmnaveengautam@gmail.com>
2026-02-26 20:01:12 +0530

    update api.ts

--- client/src/configs/api.ts
+++ client/src/configs/api.ts
@@ -5,4 +5,17 @@ const api = axios.create({
     withCredentials: true
 })
 
+api.interceptors.response.use(
+    response => response,
+    async error => {
+        const originalRequest = error.config;
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            await new Promise(resolve => setTimeout(resolve, 3000)); // 3 sec wait
+            return api(originalRequest);
+        }
+        return Promise.reject(error);
+    }
+)
+
 export default api;
