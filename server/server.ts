78d0199fe50f83ac55d31299bcee825cfeba3f9e
thenaveengautam <dmnaveengautam@gmail.com>
2026-02-26 20:30:35 +0530

    add production url

--- server/server.ts
+++ server/server.ts
@@ -23,7 +23,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'http://localhost:3000',
-        'https://thumbnailx.naveengautam.dev'
+        'https://thumbnailx-ai.vercel.app/'
     ],
     credentials: true
 }))
